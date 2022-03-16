6a4b9b9e281f1defc12e1a61769966223f90aac9
Daniil <vetusst@hotmail.com>
2022-03-16 16:20:03 +0100

    add: window width hook

--- src/hooks/useWindowWidth.js
+++ src/hooks/useWindowWidth.js
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react';
 
-export default function useWindowWidth1() {
-  const [windowWidth, setWindowWidth1] = useState(window.innerWidth);
+export default function useWindowWidth() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   useEffect(() => {
-    window.addEventListener('resize', () => setWindowWidth1(window.innerWidth));
-    return () => window.removeEventListener('resize', () => setWindowWidth1(window.innerWidth));
+    window.addEventListener('resize', () => setWindowWidth(window.innerWidth));
+    console.log('useffect')
+    return () => {
+      window.removeEventListener('resize', () => setWindowWidth(window.innerWidth))
+      console.log('clean up ')
+    };
   }, []);
 
   return windowWidth;
