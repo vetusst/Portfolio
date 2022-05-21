c9f9f52f06baa5fc83cdb776d2d960e088f382f3
Daniil <vetusst@hotmail.com>
2022-05-21 16:29:36 +0200

    add projects

--- src/res/js/projectsData.js
+++ src/res/js/projectsData.js
@@ -2,17 +2,20 @@ import weatherDesktop from '../img/weatherAppDesktop.png'
 import keyboardDesktop from '../img/keyboardDesktop.png'
 import portfolioDesktop from '../img/portfolioDesktop.png'
 import portfolioMobile from '../img/portfolioMobile.png'
-import maskWhite from '../img/iphone-mask-svg-white.svg'
+import smartHomeDesktop from '../img/smart-home-desktop.png'
+import smartHomeMobile from '../img/smart-home-mobile.png'
+import foodOrderingAppDesktop from '../img/food-ordering-app-desktop.png'
+import foodOrderingAppMobile from '../img/food-ordering-app-mobile.png'
 
 const projectsData = [
     {
-        projectName: 'Weather App',
-        desktopViewImg : weatherDesktop,
-        mobileViewImg : null,
-        projectTitle : 'Weather App',
-        projectDescription : 'Simple Weather App on Vanilla Js, with the use of a couple of APIs',
-        githubLink : 'https://github.com/vetusst/Fancy-weather',
-        deployLink : 'https://weather-fancy.netlify.app/',
+        projectName: 'Food Ordering App',
+        desktopViewImg : foodOrderingAppDesktop,
+        mobileViewImg : foodOrderingAppMobile,
+        projectTitle : 'Food Ordering App',
+        projectDescription : 'Food Ordering App on React to demonstrate usage of fundamental library features',
+        githubLink : 'https://github.com/vetusst/react-ordering-app',
+        deployLink : 'https://vetusst.github.io/react-ordering-app/',
     },
     {
         projectName: 'Portfolio',
@@ -24,6 +27,24 @@ const projectsData = [
         deployLink : 'https://dmelnikav.tech/',
     },
     {
+        projectName: 'Smart Home Interface',
+        desktopViewImg : smartHomeDesktop,
+        mobileViewImg : smartHomeMobile,
+        projectTitle : 'Smart Home Interface',
+        projectDescription : 'Smart Home demo UI on Vue.js',
+        githubLink : 'https://github.com/vetusst/smart-home-task',
+        deployLink : 'https://vetusst.github.io/smart-home-task',
+    },
+    {
+        projectName: 'Weather App',
+        desktopViewImg : weatherDesktop,
+        mobileViewImg : null,
+        projectTitle : 'Weather App',
+        projectDescription : 'Simple Weather App on Vanilla Js, with the use of a couple of APIs',
+        githubLink : 'https://github.com/vetusst/Fancy-weather',
+        deployLink : 'https://weather-fancy.netlify.app/',
+    },
+    {
         projectName: 'Virtual Keyboard',
         desktopViewImg : keyboardDesktop,
         mobileViewImg : null,
@@ -32,7 +53,5 @@ const projectsData = [
         githubLink : 'https://github.com/vetusst/Virtual-Keyboard',
         deployLink : 'https://vetusst.github.io/Virtual-Keyboard/',
     },
-    
-    
 ]
 export default projectsData
